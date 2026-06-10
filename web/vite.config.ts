3e7f986d2bda23d14fc7906e6ed57ffa8e6dd003
yang <1094963513@qq.com>
2026-06-10 09:07:44 +0800

    feat: 增加 vite 配置文件

---
+++ web/vite.config.ts
@@ -0,0 +1,33 @@
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import type { ConfigEnv, UserConfig } from 'vite'
+import { loadEnv } from 'vite'
+
+// https://vitejs.cn/config/
+const viteConfig = ({ mode }: ConfigEnv): UserConfig => {
+    const { VITE_PORT, VITE_OPEN, VITE_BASE_PATH, VITE_OUT_DIR } = loadEnv(mode, process.cwd())
+
+    return {
+        plugins: [vue()],
+        root: process.cwd(),
+        resolve: {
+            alias: {
+                '/@': resolve(__dirname, 'src'),
+            },
+        },
+        base: VITE_BASE_PATH,
+        server: {
+            port: parseInt(VITE_PORT),
+            open: VITE_OPEN != 'false',
+        },
+        build: {
+            cssCodeSplit: false,
+            sourcemap: false,
+            outDir: VITE_OUT_DIR,
+            emptyOutDir: true,
+            chunkSizeWarningLimit: 1500,
+        },
+    }
+}
+
+export default viteConfig
