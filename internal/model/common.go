698556926e15bfdc29a5ec1902838db6c6d59299
yang <1094963513@qq.com>
2026-06-19 01:17:21 +0800

    feat: 增加验证码数据模型

--- internal/model/common.go
+++ internal/model/common.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 func init() {
-	Register(&Token{})
+	Register(&Token{}, &Captcha{})
 }
 
 // Token 令牌模型，用于存储各类用户令牌
@@ -15,7 +15,21 @@ type Token struct {
 	ExpiredAt time.Time `gorm:"comment:过期时间;not null;index" json:"expired_at"`
 }
 
-// TableName 指定表名
+// TableName 指定 Token 模型表名
 func (Token) TableName() string {
 	return "tokens"
+}
+
+// Captcha 验证码模型
+type Captcha struct {
+	Key       string    `gorm:"comment:验证码查询键;type:varchar(64);primaryKey" json:"key"`
+	Code      string    `gorm:"comment:验证码值（加密后）;type:varchar(255)" json:"-"`
+	Info      string    `gorm:"comment:验证码详细信息;type:text" json:"-"`
+	ExpiredAt time.Time `gorm:"comment:过期时间;not null;index" json:"expired_at"`
+	CreatedAt time.Time `gorm:"comment:创建时间" json:"created_at"`
+}
+
+// TableName 指定 Captcha 模型表名
+func (Captcha) TableName() string {
+	return "captchas"
 }
